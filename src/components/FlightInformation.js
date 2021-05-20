ef6f9e25357a718b3f2280b55f6bfa27274452f3
Ehmad <justehmadsaeed@gmail.com>
2021-05-20 17:50:00 +0500

    📦 NEW: Add Flight information

--- src/components/FlightInformation.js
+++ src/components/FlightInformation.js
@@ -4,6 +4,20 @@ const FlightInformation = ({ flights }) => {
 	return (
 		<div id='flight-information'>
 			<h2>Flight Information</h2>
+			<div className='grid'>
+				{flights.length > 0 ? (
+					flights.map((flight, key) => (
+						<div key={key} className='info-card'>
+							<h3>{flight.code}</h3>
+							<div>Plane: {flight.plane}</div>
+							<div>Airline: {flight.airline}</div>
+							<div>Passengers: {flight.seats}</div>
+						</div>
+					))
+				) : (
+					<></>
+				)}
+			</div>
 		</div>
 	);
 };
